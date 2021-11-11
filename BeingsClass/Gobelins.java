9b96323e3cc49c14d9e40522bf126dc0a001f492
leo <leo.tillieu@smartvrac.fr>
2021-11-11 20:05:47 +0100

    move to master

--- BeingsClass/Gobelins.java
+++ BeingsClass/Gobelins.java
@@ -1,6 +1,18 @@
 package BeingsClass;
 
 public class Gobelins extends BadGuys {
+    private static MasterGobelin master;
+
+    public Gobelins(){
+        if(Gobelins.master == null){
+            Gobelins.master = new MasterGobelin();
+        }
+    }
+
+    public static MasterGobelin getMaster(){
+        return master;
+    }
+
     public void meeting(MasterGobelin g){
         g.addMessages(this.getAllMessage());
     }
